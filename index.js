94cbcd41096aa0017315edb68a0627f1331c0fc5
Anuska86 <anasrm86@gmail.com>
2024-06-01 16:32:49 +0200

    End

--- index.js
+++ index.js
@@ -33,15 +33,37 @@ app.get("/api/notes/:id", (request, response) => {
   if (note) {
     response.json(note);
   } else {
+    response.statusMessage="That note doesn't exist";
     response.status(404).end();
   }
 });
 
-app.post("/api/notes", (request, response) => {
-  const note = request.body;
-  console.log(note);
-  response.json(note);
-});
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
 
 app.delete("/api/notes/:id", (request, response) => {
   const id = Number(request.params.id);
