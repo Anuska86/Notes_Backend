47329960acfb8bdef961c9248ad89069076fdd67
Anuska86 <anasrm86@gmail.com>
2024-06-05 16:06:03 +0200

    dist

--- index.js
+++ index.js
@@ -77,5 +77,5 @@ app.delete("/api/notes/:id", (request, response) => {
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port http://localhost:${PORT}/api/notes`);
+  console.log(`Server running on port http://localhost:${PORT}`);
 });
