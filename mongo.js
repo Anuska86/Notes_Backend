dceeb0937b8ab6f0e4a446501091943587385689
Anuska86 <anasrm86@gmail.com>
2024-06-13 15:10:23 +0200

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -16,12 +16,31 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model("Note", noteSchema);
 
+
 const note = new Note({
   content: "HTML is easy",
   important: true,
 });
 
+
+/*
 note.save().then((result) => {
   console.log("note saved!");
   mongoose.connection.close();
 });
+
+*/
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
+
+/* Only important notes
+
+Note.find({ important: true }).then(result => {
+  // ...
+})
+  */
