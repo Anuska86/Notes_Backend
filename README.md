ffccc3934925093db7b47a1149be89bf3e443f50
Anuska86 <anasrm86@gmail.com>
2024-06-13 11:49:56 +0200

    Mongo

--- README.md
+++ README.md
@@ -6,8 +6,13 @@ Nodemon: npm install --save-dev nodemon
 
 CORS (Cross-Origin Resource Sharing): npm install cors
 
+Mongoose: npm install mongoose
+
 start: node index.js   or      npm start
 
 dev start: npm run dev
 
+Mongo: node mongo.js 
+
 Link: https://notesbackend-nzho.onrender.com
+
---
+++ mongo.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+var config = require('./config.json');
+console.log(config);
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@cluster0.3apkp29.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+const note = new Note({
+  content: "HTML is easy",
+  important: true,
+});
+
+note.save().then((result) => {
+  console.log("note saved!");
+  mongoose.connection.close();
+});
